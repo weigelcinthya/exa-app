1f0a0cb4bbc9de67d5d01c2d289d7fed461cc4eb
weigelcinthya <weigelcinthya@gmail.com>
2018-09-27 16:55:13 -0300

    configurando rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FeedComponent } from './feed/feed.component';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+
+const routes = [
+  {'path': 'login', component: LoginComponent}, 
+  {'path': 'cadastro-usuario', component: CadastroUsuarioComponent}, 
+  {'path': 'feed', component: FeedComponent}, 
+]
 
 @NgModule({
-  imports: [ CommonModule ],
+  imports: [ CommonModule, RouterModule.forRoot(routes) ],
   exports: [ RouterModule ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {   
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
 import { FeedComponent } from './feed/feed.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro-usuario', component: CadastroUsuarioComponent },
+  { path: 'feed', component: FeedComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Login' }
+  },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +32,10 @@ import { FeedComponent } from './feed/feed.component';
     FeedComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, BrowserAnimationsModule, RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } 
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
